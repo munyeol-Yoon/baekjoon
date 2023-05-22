3e4c99d0dce25b21b0c98035f58d69c136110744
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-05-22 14:19:04 +0900

    fst

---
+++ 9498.py
@@ -0,0 +1,12 @@
+score = input()
+
+if 90 <= int(score) <= 100:
+    print('A')
+elif 80 <= int(score) <= 89:
+    print('B')
+elif 70 <= int(score) <= 79:
+    print('C')
+elif 60 <= int(score) <= 69:
+    print('D')
+else:
+    print('F')
